cb2c947d88197da26fd5a9bd77a566d2627c2a64
TonyNogueron <antonio.nogueron03@gmail.com>
2024-03-11 14:14:20 -0600

    Adds new testing for post and JSON support

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,14 @@
-use actix_web::{dev::Server, get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{
+    dev::Server, get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder,
+};
+use serde::{Deserialize, Serialize};
 use std::net::TcpListener;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct FormData {
+    pub name: String,
+    pub email: String,
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -11,10 +20,11 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-// #[post("/echo")]
-// async fn echo(req_body: String) -> impl Responder {
-//     HttpResponse::Ok().body(req_body)
-// }
+#[post("/subscriptions")]
+async fn json_handler(data: web::Json<FormData>) -> impl Responder {
+    println!("Received JSON data: {:?}", data);
+    format!("Received JSON data: {:?}", data)
+}
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -27,6 +37,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
             .service(hello)
             .service(health_check)
             .route("/greet/{name}", web::get().to(greet))
+            .service(json_handler)
     })
     .listen(listener)?
     .run();
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -3,7 +3,7 @@
 
 use std::net::TcpListener;
 
-use actix_web::test;
+use zero_to_prod_example::FormData;
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
@@ -34,6 +34,29 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let json_body = zero_to_prod_example::FormData {
+        name: "George".to_string(),
+        email: "george_t@gmail.com".to_string(),
+    };
+
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/json")
+        .body(serde_json::to_string(&json_body).expect("Failed to parse the body to a json string"))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert_eq!(200, response.status().as_u16());
+}
 
 #[tokio::test]
 async fn subscribe_returns_a_400_code_when_data_is_missing() {
@@ -41,19 +64,55 @@ async fn subscribe_returns_a_400_code_when_data_is_missing() {
     let address = spawn_app();
     let client = reqwest::Client::new();
 
-    let test_cases = vec![
-        ("name=le%20guin", "missing the email"),
-        ("email=ursula_le_guin%40gmail.com", "missing the name"),
-        ("", "missing both name and email"),
+    let test_cases: Vec<(FormData, String)> = vec![
+        (
+            FormData {
+                name: "Le guin".to_string(),
+                email: "".to_string(),
+            },
+            "missing the email".to_string(),
+        ),
+        (
+            FormData {
+                name: "".to_string(),
+                email: "ursula_le_guin@gmail.com".to_string(),
+            },
+            "missing the name".to_string(),
+        ),
+        (
+            FormData {
+                name: "".to_string(),
+                email: "".to_string(),
+            },
+            "missing both name and email".to_string(),
+        ),
+        (
+            FormData {
+                name: "Tony".to_string(),
+                email: "not_valid_email".to_string(),
+            },
+            "has an invalid email".to_string(),
+        ),
     ];
 
     // Act
-    let response = client
-        .post(&format!("{}/subscriptions", &address))
-        .send()
-        .await
-        .expect("Failed to execute request.");
-
-    // Assert
-    assert_eq!(response.status().as_u16(), 400);
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/json")
+            .body(
+                serde_json::to_string(&invalid_body)
+                    .expect("Failed to parse the invalid body to a json string"),
+            )
+            .send()
+            .await
+            .expect("Failed to execute request.");
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
